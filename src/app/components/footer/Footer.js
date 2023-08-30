c1d6022ef0cca0c43fe86356fc0206df86ef4285
ady <ady9011@naver.com>
2023-08-30 11:18:29 +0900

    xx

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <div style={{width:'80%', margin:'auto', padding:'10px 0', textAlign:'center', backgroundColor:"lightgray"}}>
+            Copyright &copy; 2023 한국 ICT 인재개발원
+
+        </div>
+
+    );
+
+}
